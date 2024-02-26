e9091e7225abea599f40d688d029c2a0378217e7
shmoon <ssy6308msh2@naver.com>
2024-02-26 18:07:23 +0900

    35 데미지가 표시되는 기능 추가

--- Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
+++ Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
@@ -1,23 +1,20 @@
 ﻿using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
-
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
         float timeSinceLastAttack = 0;
-
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
-
             if (target == null) return;
-
             if (!GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.position);
@@ -25,18 +22,24 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
-                AttackBehavior();
+                AttackBehaviour();
             }
         }
-
-        private void AttackBehavior()
+        private void AttackBehaviour()
         {
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
+                // This will trigger the Hit() event.
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
             }
-            
+        }
+
+        // Animation Event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         private bool GetIsInRange()
@@ -52,11 +55,6 @@ namespace RPG.Combat
         {
             target = null;
         }
-
-        //Animation Event
-        void Hit()
-        {
-
-        }
+      
     }
 }
