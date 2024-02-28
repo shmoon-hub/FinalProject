3f5afc07570667862c0ab95661de4369350865dc
shmoon <ssy6308msh2@naver.com>
2024-02-28 16:33:42 +0900

    버그 수정 43

--- Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
+++ Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using RPG.Movement;
 using RPG.Core;
-
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
@@ -9,17 +8,13 @@ namespace RPG.Combat
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
-
         Health target;
         float timeSinceLastAttack = 0;
-
         private void Update()
         {
             timeSinceLastAttack += Time.deltaTime;
-
             if (target == null) return;
             if (target.IsDead()) return;
-
             if (!GetIsInRange())
             {
                 GetComponent<Mover>().MoveTo(target.transform.position);
@@ -30,21 +25,27 @@ namespace RPG.Combat
                 AttackBehaviour();
             }
         }
-
         private void AttackBehaviour()
         {
             transform.LookAt(target.transform);
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
                 // This will trigger the Hit() event.
-                GetComponent<Animator>().SetTrigger("attack");
+                TriggerAttack();
                 timeSinceLastAttack = 0;
             }
+        }
+
+        private void TriggerAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("stopAttack");
+            GetComponent<Animator>().SetTrigger("attack");
         }
 
         // Animation Event
         void Hit()
         {
+            if (target == null) { return; }
             target.TakeDamage(weaponDamage);
         }
 
@@ -52,15 +53,12 @@ namespace RPG.Combat
         {
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
-
         public bool CanAttack(CombatTarget combatTarget)
         {
-            if(combatTarget == null) { return false; }
-            
+            if (combatTarget == null) { return false; }
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
-
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
@@ -69,8 +67,14 @@ namespace RPG.Combat
 
         public void Cancel()
         {
-            GetComponent<Animator>().SetTrigger("stopAttack");
+            StopAttack();
             target = null;
+        }
+
+        private void StopAttack()
+        {
+            GetComponent<Animator>().ResetTrigger("attack");
+            GetComponent<Animator>().SetTrigger("stopAttack");
         }
     }
 }
