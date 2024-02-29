9816bd8fdbbfba5bb410787139f3905eabc1945e
shmoon <ssy6308msh2@naver.com>
2024-02-29 14:57:37 +0900

    적이 플레이어를 쫒아오도록 수정 46

--- Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
+++ Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Combat/Fighter.cs
@@ -35,42 +35,39 @@ namespace RPG.Combat
                 timeSinceLastAttack = 0;
             }
         }
-
         private void TriggerAttack()
         {
             GetComponent<Animator>().ResetTrigger("stopAttack");
             GetComponent<Animator>().SetTrigger("attack");
         }
-
         // Animation Event
         void Hit()
         {
             if (target == null) { return; }
             target.TakeDamage(weaponDamage);
         }
-
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
-        public bool CanAttack(CombatTarget combatTarget)
+
+        public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null) { return false; }
             Health targetToTest = combatTarget.GetComponent<Health>();
             return targetToTest != null && !targetToTest.IsDead();
         }
-        public void Attack(CombatTarget combatTarget)
+
+        public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
         }
-
         public void Cancel()
         {
             StopAttack();
             target = null;
         }
-
         private void StopAttack()
         {
             GetComponent<Animator>().ResetTrigger("attack");
--- Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Control/PlayerController.cs
+++ Unity RPG Project 31 인터페이스까지/Unity RPG Project/Assets/Script/Control/PlayerController.cs
@@ -1,45 +1,41 @@
-using UnityEngine;
+using System;
 using RPG.Combat;
 using RPG.Movement;
-using System;
-
+using UnityEngine;
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        private void Update() {
+        private void Update()
+        {
             if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
-           // print("Nothing to do.");
         }
-
         private bool InteractWithCombat()
         {
             RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             foreach (RaycastHit hit in hits)
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if(!GetComponent<Fighter>().CanAttack(target))
+
+                if (target == null) continue;
+
+                if (!GetComponent<Fighter>().CanAttack(target.gameObject))
                 {
                     continue;
                 }
 
-                if (target == null) continue;
-
                 if (Input.GetMouseButtonDown(0))
                 {
-                    GetComponent<Fighter>().Attack(target);
-                    
+                    GetComponent<Fighter>().Attack(target.gameObject);
                 }
                 return true;
             }
             return false;
         }
-        
         private bool InteractWithMovement()
         {
-   
-            RaycastHit hit; 
+            RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
@@ -51,10 +47,9 @@ namespace RPG.Control
             }
             return false;
         }
-
         private static Ray GetMouseRay()
         {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
-    }   
+    }
 }
